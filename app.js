f323dd6be4b3500a8195d7f6eb09ae25ce957d0c
Alexander Demin <avdemin9@yandex.ru>
2020-02-25 02:17:13 +0300

    CORS modified 27

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ const { PORT = 3000, MONGODB = MONGODEV } = process.env;
 const app = express();
 
 
-app.use((req, res, next) => {
+/* app.use((req, res, next) => {
   /* res.send({ message: req.headers, allowedCors }); */
   const { origin } = req.headers;
 
@@ -43,7 +43,7 @@ app.use((req, res, next) => {
   next();
 });
 
-app.use(cors());
+app.use(cors()); */
 app.use(helmet());
 app.use(limiter);
 app.use(cookieParser());
