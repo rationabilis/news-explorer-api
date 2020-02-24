661cdc1161180c0c8d788d4f626cce47dc89b188
Alexander Demin <avdemin9@yandex.ru>
2020-02-25 00:08:32 +0300

    CORS modified 15

--- app.js
+++ app.js
@@ -29,15 +29,15 @@ const limiter = rateLimit({
 });
 const { PORT = 3000, MONGODB = MONGODEV } = process.env;
 const app = express();
-console.log(allowedCors);
+
 app.use((req, res, next) => {
   res.send({ message: req.headers, allowedCors });
-/*   const { origin } = req.headers; */
+  const { origin } = req.headers;
 
-/*   if (allowedCors.includes(origin)) {
+  if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
-  } */
-  res.header('Access-Control-Allow-Origin', 'localhost:8080');
+  }
+  /* res.header('Access-Control-Allow-Origin', 'localhost:8080'); */
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   res.send({ message: res.headers });
