f7c18680c98ba26d582fb68347d21830bf7a97a8
Alexander Demin <avdemin9@yandex.ru>
2020-02-27 01:22:44 +0300

    CORS modified 27

--- controllers/user.js
+++ controllers/user.js
@@ -42,7 +42,7 @@ const login = (req, res, next) => {
       res
         .cookie('jwt', token, {
           maxAge: 3600000 * 24 * 7,
-          httpOnly: true,
+          /* httpOnly: true, */
           /*       sameSite: true, */
         })
         .end();
