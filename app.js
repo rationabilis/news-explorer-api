b6c3ea57dda1cc206ae8c928a87733daf0198f87
Alexander Demin <avdemin9@yandex.ru>
2020-03-03 06:54:18 +0300

    CORS modified 0203-3

--- app.js
+++ app.js
@@ -30,7 +30,11 @@ const limiter = rateLimit({
 });
 const { PORT = 3000, MONGODB = MONGODEV } = process.env;
 const app = express();
-app.use(cors());
+app.set('trust proxy', 1);
+app.use(cors(({
+  credentials: true,
+  origin: true,
+})));
 /* app.use((req, res, next) => {
   const { origin } = req.headers;
 
