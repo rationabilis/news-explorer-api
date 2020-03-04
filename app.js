e785f011dd98de9e9c17e64fe416c6c3003444b0
Alexander Demin <avdemin9@yandex.ru>
2020-03-04 18:42:00 +0300

    CORS modified 0403

--- app.js
+++ app.js
@@ -33,7 +33,7 @@ const { PORT = 3000, MONGODB = MONGODEV } = process.env;
 const app = express();
 app.options((req, res, next) => {
   const { origin } = req.headers;
-  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  /* res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept'); */
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
   if (allowedCors.includes(origin)) {
     res.header('Access-Control-Allow-Origin', origin);
