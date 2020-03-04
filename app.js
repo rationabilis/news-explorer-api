f789d7a0510ea0119066f3e390addaefba3d7b38
Alexander Demin <avdemin9@yandex.ru>
2020-03-04 18:52:57 +0300

    CORS modified 0403

--- app.js
+++ app.js
@@ -40,8 +40,8 @@ app.use((req, res, next) => {
 
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-  if (allowedCors.includes(origin)) {
-    res.header('Access-Control-Allow-Origin', origin);
+/*   if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin); */
   }
 
   next();
